d88e5572061c0838070773770d638ab8cb8845ab
David Irvine <irvined@gmail.com>
2014-08-18 11:10:00 +0100

    Added lock on openjob info, will raise exception if called twice before closing

--- openlava/openlava/lsblib.pyx
+++ openlava/openlava/lsblib.pyx
@@ -1070,6 +1070,7 @@ LSBE_LOCKED_MASTER = 130
 LSBE_DEP_ARRAY_SIZE = 131
 LSBE_NUM_ERR = 131
 
+_openjobinfocount = False
 
 def create_job_id(job_id, array_index):
     """openlava.lsblib.create_job_id(job_id, array_index)
@@ -1332,6 +1333,7 @@ Closes the connection to the MBD that was opened with lsb_openjobinfo()
     >>> lsblib.lsb_closejobinfo()
 
 """
+    _openjobinfocount = False
     openlava_base.lsb_closejobinfo()
 
 def lsb_deletejob(job_id, submit_time, options=0):
@@ -1520,6 +1522,9 @@ Get information about jobs that match the specified criteria.
 
 
 """
+    if _openjobinfocount:
+        raise Exception("closejobinfo has not been called after previous openjobinfo call")
+    _openjobinfocount = True
     cdef int numJob
     numJobs=openlava_base.lsb_openjobinfo(job_id,job_name,user,queue,host,options)
     return numJobs
